ae20b0834f3bb342cd9f21e97b44fdc4bba64004
Phoebe125 <80835102+Phoebe125@users.noreply.github.com>
2024-04-26 09:58:46 +0900

    feat: insert 수업 코드 추가

--- DML/insert.sql
+++ DML/insert.sql
@@ -11,3 +11,29 @@ insert into Subject(name, prof) values('국어', 1);
 insert into Subject(name, prof) values('수학', 2), ('과학', 2), ('미술', 2);
 select * from Subject;
 
+desc Student;
+
+insert into Student(name, birthdt, major, mobile, email) 
+	values ('홍길동', '2002-01-02', 1, '01012341234', 'hong@gmail.com');
+
+insert into Student(name, birthdt, major, mobile, email) values
+	('박길동', '2003-01-22', 1, '01012341234', 'park@gmail.com'),
+    ('김길동', '2002-01-22', 1, '01012341234', 'kim@gmail.com');
+    
+select * from Student;
+
+insert into Enroll(subject, student) values(1, 1);
+insert into Enroll(subject, student) values(2, 2);
+-- Error Code: 1062. Duplicate entry '2-2' for key 'enroll.unique_subject_student'
+insert into Enroll(subject, student) values(2, 2);
+
+-- 해결 방법
+insert ignore into Enroll(subject, student) values
+	(2, 1), (2, 2); -- 중복인 경우 무시~ (id=3 없음)
+    
+-- : 2 row(s) affected: insert 하다가 중복이 발생한 행에 대해서는 update를 할 것임
+insert into Enroll(subject, student) values (2, 2)
+	on duplicate key update createdate = now();
+
+select * from Enroll;
+
