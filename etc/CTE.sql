0fb1d90ca9ba2a68e1070e6ac346cd90a04315e1
Phoebe125 <80835102+Phoebe125@users.noreply.github.com>
2024-04-29 13:56:51 +0900

    feat: 재귀함수 예시 추가

--- etc/CTE.sql
+++ etc/CTE.sql
@@ -47,3 +47,13 @@ WITH RECURSIVE fibonacci (n, fib_n, next_fib_n) AS
 )
 select * from fibonacci;
 
+-- 재귀함수의 또다른 예시
+WITH RECURSIVE CteDept(id, pid, pname, dname, d, h) AS 
+(
+    select id, pid, cast('' as char(31)), dname, 0, cast(id as char(10)) from Dept where pid = 0
+    UNION ALL
+    select d.id, d.pid, cte.dname, d.dname, d + 1, concat(cte.h, '-', d.id) from Dept d inner join CteDept cte on d.pid = cte.id
+)
+select /*+ SET_VAR(cte_max_recursion_depth = 5) */ d, dname, h from CteDept order by h;
+
+
