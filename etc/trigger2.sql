7293618cde8c0fa936492ae8cb0f7e90239521ff
Phoebe125 <80835102+Phoebe125@users.noreply.github.com>
2024-04-29 09:54:30 +0900

    feat: insert, delete, update trigger 생성

---
+++ etc/trigger2.sql
@@ -0,0 +1,50 @@
+use testdb;
+
+select * from Subject;
+select * from Prof; -- 지난번에 해당 교수가 담당하는 과목 수 까지 진행했음
+
+-- Subject에 insert 문이 실행되면, 해당 교수의 subjectcnt를 하나 증가되도록
+-- Trigger 만들어 보기
+create trigger tr_Subject_after_insert
+		after insert on Subject for each row
+		update Prof set subjectcnt = subjectcnt + 1
+        where id = New.prof;
+    
+insert into Subject(name, prof) values('사회', 4);
+
+-- Subject에 delete 문이 실행될때의 Trigger 생성
+create trigger tr_Subject_after_delete
+		after delete on Subject for each row
+		update Prof set subjectcnt = subjectcnt - 1
+        where id = OLD.prof;
+        
+delete from Subject where id = 7;
+
+-- Subject에 update문이 실행될 때의 Trigger 생성
+select prof, count(*)
+	from Subject
+    group by prof;
+
+-- 한개의 Table에 update가 일어나면, 이에 다른 Table도 동일한 값으로 update가 일어나도록
+update Prof p
+	inner join (select prof, count(*) cnt from Subject group by prof) sub
+		on p.id = sub.prof
+	set p.subjectcnt = sub.cnt
+    where p.id > 0;
+    
+Select s.*, p.subjectcnt
+	from Subject s inner join Prof p on p.id = s.prof;
+
+create trigger tr_subject_after_update
+	after update on Subject for each row
+	update Prof set subjectcnt = if(id = NEW.prof, subjectcnt + 1, subjectcnt - 1)
+	where id in (OLD.prof, NEW.prof) and OLD.prof <> NEW.prof; -- prof에 변경이 있을 때
+-- trigger는 가벼워야한다.
+    
+select * from Subject;
+select * from Prof;
+update Subject set prof = 8 where id = 4;
+update Subject set name = '사회과부도' where id = 8;
+
+    
+    
---
+++ etc/trigger3.sql
@@ -0,0 +1,47 @@
+-- p.52에 있는 실습
+use schooldb;
+
+-- 1. Dept 테이블에 empcnt 컬럼 추가
+alter table Dept add column empcnt smallint unsigned not null default 0 comment '부서별 직원수';
+
+select * from Dept;
+select * from Emp;
+
+-- 2. insert trigger 생성 (Emp insert하면, empcnt 를 증가)
+create trigger tr_Emp_after_insert
+		after insert on Emp for each row
+		update Dept set empcnt = empcnt + 1
+        where id = New.dept;
+
+-- 잘 돌아가는 지 insert 문 생성
+insert into Emp(ename, dept, salary) values('이선민', 4, 300);    
+
+-- 3. delete trigger 생성 (Emp delete하면, empcnt를 감소)
+create trigger tr_Emp_after_delete
+		after delete on Emp for each row
+		update Dept set empcnt = empcnt - 1
+        where id = Old.dept;
+        
+-- 잘 돌아가는 지 delete 문 생성        
+delete from Emp where id = 253;
+    
+-- 4. 초기값 세팅
+select * from Dept;
+
+update Dept d
+	inner join (select dept, count(*) cnt from Emp group by dept) e
+		on d.id = e.dept
+	set d.empcnt = e.cnt
+    where d.id > 0;
+    
+-- 5. update 문 작성
+create trigger tr_emp_after_update
+	after update on Emp for each row
+	update Dept set empcnt = if(id = NEW.dept, empcnt + 1, empcnt - 1)
+	where id in (OLD.dept, NEW.dept) and OLD.dept <> NEW.dept; -- dept에 변경이 있을 때
+    
+-- 잘 돌아가는지 update 확인
+select * from Emp;
+update Emp set dept = 5 where id = 3;
+
+    
